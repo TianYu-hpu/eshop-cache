81e4730c4bd5ad7b5cdb2a08d9f6ca9ea5d48129
tianyu <tianyu.hpu@gmail.com>
2019-01-10 22:41:55 +0800

    zookeeper 分布式锁

---
+++ src/main/java/com/roncoo/eshop/cache/constants/Constants.java
@@ -0,0 +1,12 @@
+package com.roncoo.eshop.cache.constants;
+
+/**
+ * @Auther: tianyu
+ * @Date: 2019/1/10 22:34
+ * @Description:  常量池
+ */
+public interface Constants {
+
+    String PRODUCT_DISTRIBUTE_LOCK_PREFIX = "/product-lock-";
+
+}
